cda4db223590101c66201f418c205adc0dde3e36
Douglas Gordon <douglas.gordon@vroom.com>
2017-05-25 16:53:47 -0400

    skew every page

---
+++ skew.js
@@ -0,0 +1,8 @@
+const allElements = Array.from(document.all)
+
+setInterval(() => {
+  allElements.forEach((o) => {
+    const rand = Math.floor(Math.random() * 11 - 5)
+    o.style.transform = `rotate(${rand}deg)`
+  })
+}, 1000)
